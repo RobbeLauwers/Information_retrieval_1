a8ab703b2d704a3c6579ac01d83b3ea9b3253fc9
RobbeLauwers <robbe.lauwers@student.uantwerpen.be>
2021-11-01 23:34:43 +0100

    Read queries

--- Lucene_code/src/mainClass.java
+++ Lucene_code/src/mainClass.java
@@ -17,10 +17,7 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -28,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 // Only use import below if junit is actually installed, might not be useful
 // import static org.junit.Assert.assertEquals;
@@ -104,8 +102,23 @@ public class mainClass {
         readFiles(iwriter, true);
     }
 
-    public static void queryProcessing(){
-
+    // Parse .tsv files (queries)
+    // Return format: data.get(x) gets row x, data.get(x)[0] gets query number, data.get(x)[1] gets query
+    // https://stackoverflow.com/a/61443651
+    public static ArrayList<String[]> tsvr(File test2) {
+        ArrayList<String[]> Data = new ArrayList<>(); //initializing a new ArrayList out of String[]'s
+        try (BufferedReader TSVReader = new BufferedReader(new FileReader(test2))) {
+            String line = null;
+            while ((line = TSVReader.readLine()) != null) {
+                String[] lineItems = line.split("\t"); //splitting the line and adding its items in String[]
+                Data.add(lineItems); //adding the splitted line array to the ArrayList
+            }
+            // Remove header row
+            Data.remove(0);
+        } catch (Exception e) {
+            System.out.println("Query file could not be parsed");
+        }
+        return Data;
     }
 
     public static void main(String[] args) throws IOException, ParseException {
@@ -135,11 +148,16 @@ public class mainClass {
         // We want to search in the index we just read
         IndexSearcher isearcher = new IndexSearcher(ireader);
 
-        // full contents of file 1537
+        // full contents of file 1537, used to test query
         String fileContent1537 = "\"Forced air vs central air: a comparison guideTags: HVAC, install central A/CCentral air, often confused as an alternative to forced-air systems, is actually an additional component that can be added to them.Do you like this article? TweetQuality HVAC ServicesWhere do you need HVAC services?GoWhere do you need HVAC services?GoFeatured inForced air is an HVAC system designed to push air through the ductwork of a home or an office building. Central air applies only to air conditioning and is often, but not always, included within forced-air systems. Read on for the basics of forced-air vs. central air systems.Get free quotes on forced air versus central air today!Forced airForced-air systems can be found in most residential and corporate buildings. These systems use ducting to efficiently deliver heating and cooling to where it is needed. A forced AC unit is one of the most popular types as nearly all contractors are familiar with this system.When it comes to cost, forced-air vs. central air conditioning depends largely on the size of the house, the quality of the ducting and the AC unit itself. Be sure to hire a contractor who is well known in your area to provide you with a forced-air system estimate.Before making a purchase, consider the following aspects of forced-air AC units:Price range from $711 to $1,192.Easy to install.Comes with energy-efficient options.Comes with heating and cooling elements.The question of forced air vs. central air is really about whether a basic system is sufficient or whether the convenience of central air justifies the additional costs.Central airCentral air, often confused as an alternative to forced-air systems, is actually an additional component that can be added to the existing forced-air system. The central air conditioning unit is for cooling only. It uses a condenser and a complex series of coils to send blasts of cold air throughout the entire building.The condenser portion of a central air unit is located outside the home or office building. It converts air from the environment into cold air.You should hire a contractor to install an AC unit, such as central air.Central air units range in price from $3,500 to $4,000 for an average-sized home of around 2,000 square feet. So, it is important to remember that this cost is in addition to the initial cost of a forced-air system.One of the primary benefits of a central air system is that, unlike other HVAC systems, it delivers cool air to the entire house almost instantly. For a lot of homeowners, this convenience is worth the cost. Central air is an especially popular addition to homes located in parts of the country with warm and humid climates.WarrantyDue to their complex system of coils, central air units do tend to require maintenance as time passes. For this reason, it is recommended you purchase a central air system that comes with a lifetime warranty. Although such warranties may add to the upfront cost of a unit, they are a great way to save money on replacements and repairs over the lifetime of your product.The forced air vs. central air debate is really more a question about cost and convenience. Central air is expensive, but the convenience of instantly cooling your home may compensate for the additional cost.Photo credit: mira_foto via Compfight CC.Share This Article\"\n";
 
-        // Full contents of file 1. This crashes if actually used as a query if not escaped (See "Query query = ..." below)
+        // Full contents of file 1. This crashes if used as a query if not escaped (See "Query query = ..." below)
         String fileContent1 = "Science & Mathematics PhysicsThe hot glowing surfaces of stars emit energy in the form of electromagnetic radiation.?It is a good approximation to assume that the emissivity e is equal to 1 for these surfaces.  Find the radius of the star Rigel, the bright blue star in the constellation Orion that radiates energy at a rate of 2.7 x 1032 W and has a surface temperature of 11,000 K. Assume that the star is spheriw moreFollow 3 answersAnswersRelevanceRatingNewestOldestBest Answer: Stefan-Boltzmann law states that the energy flux by radiation is proportional to the forth power of the temperdecade ago0 18 CommentSchmiso, you forgot a 4 in your answer. Your link even says iosity, as the energy in this problem, you can find the radius R by doingope this helps everyone.Caroline · 4 years ago4 1 Comment (Stefan-Boltng in your values you should get: ou would like to learn more about one of these?Want to build a free website? Interested in dating sites?Need a Home Security Safe? How to order contacts online?\n";
+
+        // read query file from path
+        File queryFile = new File("./Queries/dev_queries.tsv");
+        // Parse queries: queries.get(x) gets row x, queries.get(x)[0] gets query number, queries.get(x)[1] gets query
+        ArrayList<String[]> queries = tsvr(queryFile);
 
         // Query the contents of the first file
         // "file_content" is the name of the field we want to search. It is defined like this during index creation (see method readFiles)
@@ -163,6 +181,7 @@ public class mainClass {
         for (int i = 0; i < hits.length; i++) {
             // Get one result
             Document hitDoc = isearcher.doc(hits[i].doc);
+
             System.out.print("Rank number: " + String.valueOf(i + 1) + ", Doc number: " + hitDoc.getField("file_number") + "\n");
             //System.out.print("Doc content: " + hitDoc.getField("file_content") + "\n");
         }
