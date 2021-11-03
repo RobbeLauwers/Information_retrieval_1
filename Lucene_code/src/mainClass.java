7e4e4de52ba0bd5b694ea65f20e69146d87300b4
RobbeLauwers <robbe.lauwers@student.uantwerpen.be>
2021-11-03 20:26:28 +0100

    Index + search full dataset

--- Lucene_code/src/mainClass.java
+++ Lucene_code/src/mainClass.java
@@ -33,6 +33,8 @@ import java.util.stream.Collectors;
 // https://lucene.apache.org/core/7_3_1/core/index.html
 // TODO: split up indexing/reading queries/querying into separate classes/files?
 public class mainClass {
+    public static long start;
+    public static long startTime;
 
     // https://zetcode.com/java/listdirectory/
     // Prints all files in directory
@@ -53,6 +55,13 @@ public class mainClass {
         } else {
             dirName = "./Datasets/Large";
         }
+
+        //TODO verwijder
+        dirName = "./Datasets/Large/full_docs/full_docs";
+
+        start = System.currentTimeMillis();
+        startTime = System.currentTimeMillis();
+
         //For each file in dataset
         Files.list(new File(dirName).toPath())
                 //Get the path to the file
@@ -64,6 +73,13 @@ public class mainClass {
                             doc.add(new Field("file_number", path.toString().replaceAll("\\D+", ""), TextField.TYPE_STORED));
                             // Initialize file contents
                             String content = "";
+
+                            //TODO: verwijder
+                            if (iwriter.numDocs()%10000 == 0){
+                                System.out.print("Indexed " + iwriter.numDocs() + " files, time since last update: " + (-startTime+System.currentTimeMillis()) + "\n");
+                                startTime = System.currentTimeMillis();
+                            }
+
                             // Exception handling seems to be mandatory?
                             try {
                                 // Get file from path
@@ -122,10 +138,11 @@ public class mainClass {
     }
 
     public static void main(String[] args) throws IOException, ParseException {
+
         // Store the index in memory:
-        Directory directory = new RAMDirectory();
+        //Directory directory = new RAMDirectory();
         // To store an index on disk, use this instead:
-        //Directory directory = FSDirectory.open("/tmp/testindex");
+        Directory directory = FSDirectory.open(Paths.get("./tmp/testindex"));
 
         // TODO: move the analyzer/index definitions below to the indexing() method?
 
@@ -142,49 +159,53 @@ public class mainClass {
         // Indexing finished -> close index writer
         iwriter.close();
 
+        System.out.print("Indexing took " + (-start + System.currentTimeMillis()));
+
         // Now search the index:
         // Open the directory where we stored the index
         DirectoryReader ireader = DirectoryReader.open(directory);
         // We want to search in the index we just read
         IndexSearcher isearcher = new IndexSearcher(ireader);
 
-        // full contents of file 1537, used to test query
-        String fileContent1537 = "\"Forced air vs central air: a comparison guideTags: HVAC, install central A/CCentral air, often confused as an alternative to forced-air systems, is actually an additional component that can be added to them.Do you like this article? TweetQuality HVAC ServicesWhere do you need HVAC services?GoWhere do you need HVAC services?GoFeatured inForced air is an HVAC system designed to push air through the ductwork of a home or an office building. Central air applies only to air conditioning and is often, but not always, included within forced-air systems. Read on for the basics of forced-air vs. central air systems.Get free quotes on forced air versus central air today!Forced airForced-air systems can be found in most residential and corporate buildings. These systems use ducting to efficiently deliver heating and cooling to where it is needed. A forced AC unit is one of the most popular types as nearly all contractors are familiar with this system.When it comes to cost, forced-air vs. central air conditioning depends largely on the size of the house, the quality of the ducting and the AC unit itself. Be sure to hire a contractor who is well known in your area to provide you with a forced-air system estimate.Before making a purchase, consider the following aspects of forced-air AC units:Price range from $711 to $1,192.Easy to install.Comes with energy-efficient options.Comes with heating and cooling elements.The question of forced air vs. central air is really about whether a basic system is sufficient or whether the convenience of central air justifies the additional costs.Central airCentral air, often confused as an alternative to forced-air systems, is actually an additional component that can be added to the existing forced-air system. The central air conditioning unit is for cooling only. It uses a condenser and a complex series of coils to send blasts of cold air throughout the entire building.The condenser portion of a central air unit is located outside the home or office building. It converts air from the environment into cold air.You should hire a contractor to install an AC unit, such as central air.Central air units range in price from $3,500 to $4,000 for an average-sized home of around 2,000 square feet. So, it is important to remember that this cost is in addition to the initial cost of a forced-air system.One of the primary benefits of a central air system is that, unlike other HVAC systems, it delivers cool air to the entire house almost instantly. For a lot of homeowners, this convenience is worth the cost. Central air is an especially popular addition to homes located in parts of the country with warm and humid climates.WarrantyDue to their complex system of coils, central air units do tend to require maintenance as time passes. For this reason, it is recommended you purchase a central air system that comes with a lifetime warranty. Although such warranties may add to the upfront cost of a unit, they are a great way to save money on replacements and repairs over the lifetime of your product.The forced air vs. central air debate is really more a question about cost and convenience. Central air is expensive, but the convenience of instantly cooling your home may compensate for the additional cost.Photo credit: mira_foto via Compfight CC.Share This Article\"\n";
-
-        // Full contents of file 1. This crashes if used as a query if not escaped (See "Query query = ..." below)
-        String fileContent1 = "Science & Mathematics PhysicsThe hot glowing surfaces of stars emit energy in the form of electromagnetic radiation.?It is a good approximation to assume that the emissivity e is equal to 1 for these surfaces.  Find the radius of the star Rigel, the bright blue star in the constellation Orion that radiates energy at a rate of 2.7 x 1032 W and has a surface temperature of 11,000 K. Assume that the star is spheriw moreFollow 3 answersAnswersRelevanceRatingNewestOldestBest Answer: Stefan-Boltzmann law states that the energy flux by radiation is proportional to the forth power of the temperdecade ago0 18 CommentSchmiso, you forgot a 4 in your answer. Your link even says iosity, as the energy in this problem, you can find the radius R by doingope this helps everyone.Caroline · 4 years ago4 1 Comment (Stefan-Boltng in your values you should get: ou would like to learn more about one of these?Want to build a free website? Interested in dating sites?Need a Home Security Safe? How to order contacts online?\n";
-
         // read query file from path
         File queryFile = new File("./Queries/dev_queries.tsv");
         // Parse queries: queries.get(x) gets row x, queries.get(x)[0] gets query number, queries.get(x)[1] gets query
         ArrayList<String[]> queries = tsvr(queryFile);
 
-        // Query the contents of the first file
         // "file_content" is the name of the field we want to search. It is defined like this during index creation (see method readFiles)
         // Currently, we use the same analyzer that was used to create the index (remove/keep stop words from query, etc), probably possible to use a different one
         QueryParser parser = new QueryParser("file_content", analyzer);
 
-        // Apply analyzer to query, removing stop words etc depending on analyzer settings
-        // QueryParser.escape Changes special characters so that they do not crash the query
-        // https://stackoverflow.com/a/10259944
-        // TODO: using QueryParser.escape strongly increases the amount of results, should this happen? Is it even
-        //  needed to do this, do any queries from the assignment crash if not escaped?
-        Query query = parser.parse(QueryParser.escape(fileContent1537));
+        int counter = 0;
 
-        // Actually execute the query, limit shown results to 10
-        ScoreDoc[] hits = isearcher.search(query, 10, Sort.RELEVANCE).scoreDocs;
+        // Reminder: query[0] gets query number, query[1] gets query
+        for ( String[] queryRow : queries){
+            // Apply analyzer to query, removing stop words etc depending on analyzer settings
+            // QueryParser.escape Changes special characters so that they do not crash the query
+            // https://stackoverflow.com/a/10259944
+            // TODO: using QueryParser.escape strongly increases the amount of results, should this happen? Is it even
+            //  needed to do this, do any queries from the assignment crash if not escaped?
+            Query query = parser.parse(QueryParser.escape(queryRow[1]));
 
-        // example of how junit can be used to test results (needs junit jar to be added to project)
-        //assertEquals(1, hits.length);
+            counter += 1;
+            if ((counter % 100) == 0){
+                System.out.print("Running query " + counter + "\n");
+            }
 
-        // Iterate through the results:
-        for (int i = 0; i < hits.length; i++) {
-            // Get one result
-            Document hitDoc = isearcher.doc(hits[i].doc);
+            // Actually run the query, limit shown results to 10
+            ScoreDoc[] hits = isearcher.search(query, 10, Sort.RELEVANCE).scoreDocs;
 
-            System.out.print("Rank number: " + String.valueOf(i + 1) + ", Doc number: " + hitDoc.getField("file_number") + "\n");
-            //System.out.print("Doc content: " + hitDoc.getField("file_content") + "\n");
+            // Iterate through the results:
+            for (int i = 0; i < hits.length; i++) {
+                // Get one result
+                Document hitDoc = isearcher.doc(hits[i].doc);
+
+                // System.out.print("Rank number: " + String.valueOf(i + 1) + ", Doc number: " + hitDoc.getField("file_number") + "\n");
+                //System.out.print("Doc content: " + hitDoc.getField("file_content") + "\n");
+            }
         }
+
+
 
         ireader.close();
         directory.close();
--- README.md
+++ README.md
@@ -3,20 +3,24 @@
 ## Installation
 
 Make sure the [small dataset](https://drive.google.com/file/d/1_8np3rVc8WIknYLr9G2e1gSr2lRPVT60/view) 's text files are
-in [./Datasets/Small](./Datasets/Small)  (not included in the git repository, not sure if it has to be included here? 
-Large dataset will be 1.4 GB compressed, seemed too large to include)
+in [./Datasets/Small](./Datasets/Small)
+, [large dataset](https://drive.google.com/file/d/17Fyu7y7fx6Z6RptxiUbioqmjBhXOp2HN/view)'s text files should be
+in [./Datasets/Large/full_docs/full_docs](./Datasets/Large/full_docs/full_docs)  (
+not included in the git repository, not sure if it has to be included here? Large dataset will be 1.4 GB compressed,
+seemed too large to include)
 
-Make sure Lucene is added to the project before running. Version 
-[7_3_1](https://archive.apache.org/dist/lucene/java/7.3.1/) is used. The jar files needed are 
-core, queryParser and misc (Should the lucene jars be included on github?)
+Make sure Lucene is added to the project before running. Version
+[7_3_1](https://archive.apache.org/dist/lucene/java/7.3.1/) is used. The jar files needed are core, queryParser and
+misc (Should the lucene jars be included on github?)
 
-Adding jars using intelliJ: 
- - Top left -> File
- - Project Structure
- - left sidebar: Modules
- - Select Lucene_code in the middle column
- - On the right, select dependencies
- - Look for a +
- - JARs or directories
- - Select needed jar files
+Adding jars using intelliJ:
+
+- Top left -> File
+- Project Structure
+- left sidebar: Modules
+- Select Lucene_code in the middle column
+- On the right, select dependencies
+- Look for a +
+- JARs or directories
+- Select needed jar files
 
